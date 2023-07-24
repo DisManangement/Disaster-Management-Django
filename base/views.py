010d17a5da72d765db5ab83b455e4c8279facd19
febin <febnmathew98@gmail.com>
2023-07-24 17:46:53 +0530

    edit user\ error:email and username validation

--- base/views.py
+++ base/views.py
@@ -15,9 +15,13 @@ def home(request):
 
 
 def stateCommittee(request):
+
+    states = State.objects.all()
+
+    context = {'states' :states,}
     
 
-    return render(request, 'base/stateCommittee/viewStateCommittee.html')
+    return render(request, 'base/stateCommittee/viewStateCommittee.html', context)
 
 def volunteer(request):
 
@@ -47,7 +51,9 @@ def addVolunteer(request):
 
 
 
+"""---------- STATE CRUD OPERATIONS -----------"""
 
+"""--- CREATE NEW STATE --- """
 
 def createState(request):
 
@@ -58,6 +64,7 @@ def createState(request):
         password = request.POST.get('password')
         phone = request.POST.get('number')
         state = request.POST.get('state')
+        location = request.POST.get('location')
 
         if User.objects.filter(email=email).exists():
             messages.info(request, 'Email Taken')
@@ -70,15 +77,74 @@ def createState(request):
              user = User.objects.create_user(username=username, email=email, password=password)
              user.save()
 
-             State.objects.create(host=user,name=username,phone=phone, state=state)
+             State.objects.create(host=user,name=username,phone=phone, state=state, location=location)
 
              return redirect('statecommittee') 
       
 
+""" --- EDIT EXISTING STATE --- """
 
-def editState(request):
+
+def editState(request,pk):
+    state = State.objects.get(id=pk)
+    user = state.host
+
+    print('user',user)
+    
+    form = StateForm(instance=state)
+
+
+    if request.method == 'POST':
+       
+        email = request.POST.get('email')
+        username = request.POST.get('name')
+        tempUser = User.objects.filter(email=email)
+        tempEmail = tempUser.username
+        print('temp email', tempEmail)
+        
+        if User.objects.filter(email=email).exists():
+            
+            print('temp',tempUser)
+            if user.email != User.objects.filter(email=email) :
+
+             messages.info(request, 'Email Already Taken')
+             return redirect(f'/editstate/{pk}')
+            
+        elif  User.objects.filter(username=username).exists():
+            if user != User.objects.filter(username=username) :
+                messages.info(request, 'Username Already Taken')
+        
+        else:
+                  state.name= request.POST.get('name')
+                  state.phone = request.POST.get('number')
+                  state.state = request.POST.get('state')
+                  state.location = request.POST.get('location')
+
+                  user.email = email
+                  user.username = username
+                  user.password= request.POST.get('password')
+        
+                  state.save()
+                  user.save()
+        
+                  return redirect('statecommittee')
+
+    context ={'user':user, 'state':state}
+
+    return render(request, 'base/stateCommittee/editState.html', context)
+
+        
+          
+
+""" --- DELETE EXISTING STATE --- """
+
+def deleteState(request):
     pass
         
+
+
+
+""" <----------  STATE ENDS -------> """
 
   
   
