799961d8bbbc2f139146f8472ebac745890c0923
febin <febnmathew98@gmail.com>
2023-07-24 21:16:55 +0530

    finished:Edit Models  Bugfix: email\username validation

--- base/views.py
+++ base/views.py
@@ -84,7 +84,7 @@ def addVolunteer(request):
 
 """---------- STATE CRUD OPERATIONS -----------"""
 
-"""--- CREATE NEW STATE --- """
+# --- CREATE NEW STATE ---
 
 def createState(request):
 
@@ -113,73 +113,71 @@ def createState(request):
              return redirect('statecommittee') 
       
 
-""" --- EDIT EXISTING STATE --- """
+# --- EDIT EXISTING STATE ---
 
 
-def editState(request,pk):
-    state = State.objects.get(id=pk)
+def editState(request, pk):
+    state = State.objects.select_related('host').get(id=pk)
     user = state.host
 
-    print('user',user)
-    
+    print('user', user)
+
     form = StateForm(instance=state)
 
-
     if request.method == 'POST':
-       
         email = request.POST.get('email')
         username = request.POST.get('name')
-        tempUser = User.objects.filter(email=email)
-        tempEmail = tempUser.username
-        print('temp email', tempEmail)
-        
-        if User.objects.filter(email=email).exists():
-            
-            print('temp',tempUser)
-            if user.email != User.objects.filter(email=email) :
 
-             messages.info(request, 'Email Already Taken')
-             return redirect(f'/editstate/{pk}')
-            
-        elif  User.objects.filter(username=username).exists():
-            if user != User.objects.filter(username=username) :
-                messages.info(request, 'Username Already Taken')
-        
-        else:
-                  state.name= request.POST.get('name')
-                  state.phone = request.POST.get('number')
-                  state.state = request.POST.get('state')
-                  state.location = request.POST.get('location')
+        # Check if the given email is already taken by another user
+        existing_user = User.objects.filter(email=email).exclude(pk=user.pk).first()
+        if existing_user:
+            messages.info(request, 'Email Already Taken')
+            return redirect(f'/editstate/{pk}')
 
-                  user.email = email
-                  user.username = username
-                  user.password= request.POST.get('password')
-        
-                  state.save()
-                  user.save()
-        
-                  return redirect('statecommittee')
+        # Check if the given username is already taken by another user
+        existing_user = User.objects.filter(username=username).exclude(pk=user.pk).first()
+        if existing_user:
+            messages.info(request, 'Username Already Taken')
+            return redirect(f'/editstate/{pk}')
 
-    context ={'user':user, 'state':state}
+        # Update state and user data
+        state.name = request.POST.get('name')
+        state.phone = request.POST.get('number')
+        state.state = request.POST.get('state')
+        state.location = request.POST.get('location')
+
+        user.email = email
+        user.username = username
+        user.set_password(request.POST.get('password'))
+
+        state.save()
+        user.save()
+
+        return redirect('statecommittee')
+
+    context = {'user': user, 'state': state}
 
     return render(request, 'base/stateCommittee/editState.html', context)
 
+# --- DELETE EXISTING STATE --- 
+
+def deleteState(request,pk):
+    
+    state = State.objects.get(id=pk)
+    state.delete()
+    return redirect('statecommittee')
+    
         
-          
-
-""" --- DELETE EXISTING STATE --- """
-
-def deleteState(request):
-    pass
-        
 
 
 
-""" <----------  STATE ENDS -------> """
+#   ----------  STATE ENDS ------->
 
   
 
 """ ---------- VOLUNTEER CRUD OPERATIONS ------ """
+
+#  -----     CREATE VOLUNTEER      ------
 
 
 def createVolunteer(request):
@@ -203,11 +201,116 @@ def createVolunteer(request):
            Volunteer.objects.create(host=user, name=username, phone=phone, state=state, location=location)
 
            return redirect('volunteer')
+       
+
+ # -----     DELETE EXISTING VOLUNTEER     -----
+
+def deleteVolunteer(request, pk):
+    volunteer = Volunteer.objects.get(id=pk)
+    volunteer.delete()
+
+    return redirect('volunteer')
+
+
+
+#  ----- EDIT VOLUNTEER  -----
+
+
+def editVolunteer(request, pk):
+    volunteer = Volunteer.objects.select_related('host').get(id=pk)
+    user = volunteer.host
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('name')
+
+        # Check if the given email is already taken by another user
+        existing_user = User.objects.filter(email=email).exclude(pk=user.pk).first()
+        if existing_user:
+            messages.info(request, 'Email Already Taken')
+            return redirect(f'/editvolunteer/{pk}')
+
+        # Check if the given username is already taken by another user
+        existing_user = User.objects.filter(username=username).exclude(pk=user.pk).first()
+        if existing_user:
+            messages.info(request, 'Username Already Taken')
+            return redirect(f'/editvolunteer/{pk}')
+
+        # Update state and user data
+        volunteer.name = request.POST.get('name')
+        volunteer.phone = request.POST.get('number')
+        volunteer.state = request.POST.get('state')
+        volunteer.location = request.POST.get('location')
+
+        user.email = email
+        user.username = username
+        user.set_password(request.POST.get('password'))
+
+        volunteer.save()
+        user.save()
+
+        return redirect('volunteer')
+
+    context = {'user': user, 'volunteer': volunteer}
+
+    return render(request, 'base/volunteer/editVolunteer.html', context)
            
 
    
    
    
+"""  ----  END USER CRUD OPERATIONS    ----- """
+
+# ----    DELETE EXISTING USER    ---- 
+
+
+def deleteUser(request, pk):
+    user = EndUser.objects.get(id=pk)
+    user.delete()
+    return redirect('/')
+
+
+
+def editEndUser(request, pk):
+    enduser = EndUser.objects.select_related('host').get(id=pk)
+    user = enduser.host
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('name')
+
+        # Check if the given email is already taken by another user
+        existing_user = User.objects.filter(email=email).exclude(pk=user.pk).first()
+        if existing_user:
+            messages.info(request, 'Email Already Taken')
+            return redirect(f'/edituser/{pk}')
+
+        # Check if the given username is already taken by another user
+        existing_user = User.objects.filter(username=username).exclude(pk=user.pk).first()
+        if existing_user:
+            messages.info(request, 'Username Already Taken')
+            return redirect(f'/edituser/{pk}')
+
+        # Update state and user data
+        enduser.name = request.POST.get('name')
+        enduser.phone = request.POST.get('number')
+        enduser.state = request.POST.get('state')
+        enduser.location = request.POST.get('location')
+
+        user.email = email
+        user.username = username
+        user.set_password(request.POST.get('password'))
+
+        enduser.save()
+        user.save()
+
+        return redirect('/')
+
+    context = {'user': user, 'enduser': enduser}
+
+    return render(request, 'base/user/editEndUser.html', context)
+
+
 
 
   
