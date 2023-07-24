0cc48c6c5378719850e1e8e484029b5e6cc836b1
febin <febnmathew98@gmail.com>
2023-07-24 14:02:09 +0530

    Add state view created

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Login , State
+from .models import State
+
 
 # Register your models here.
 
-admin.site.register(Login)
 admin.site.register(State)
